7d6ea5c0934a414e1ed97ab9224906dd3b2054f4
seneye30 <olverreroma@gmail.com>
2024-05-14 23:50:04 +0800

    first commit

---
+++ urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+   
+    path('admin/', admin.site.urls),
+
+    path('', include('book.urls')),
+    path('studentlist/', include('student.urls')),
+    path('', include(('library.urls','library'),'library')),
+    
+    path("__reload__/", include("django_browser_reload.urls")),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL)
